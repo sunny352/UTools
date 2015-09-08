3d0f38cfa5c52d4e523de96ed4fafaed1dc3056d
sunny352@gmail.com <sunny352@gmail.com>
2015-09-08 18:31:58 +0800

    添加ShowProfile，用于显示游戏运行性能

---
+++ ShowProfile.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+//用于显示游戏运行帧率和内存占用
+public static class ShowProfile
+{
+	public static float SampleTime = 0.5f;
+	private static float m_lastUpdateTime = 0.0f;
+	private static float m_fps = 0.0f;
+	private static int m_frame = 0;
+	public static void Update()
+	{
+		++m_frame;
+	}
+	public static void FixedUpdate()
+	{
+		float currentTime = Time.time;
+		float flameTime = currentTime - m_lastUpdateTime;
+		if (flameTime >= SampleTime)
+		{
+			m_fps = m_frame / flameTime;
+			m_lastUpdateTime = currentTime;
+			m_frame = 0;
+			long monoMemory = System.GC.GetTotalMemory(false);
+			uint totalMemory = Profiler.GetTotalAllocatedMemory();
+			m_info = string.Format("Total:{0:0.0}MB\nMono:{1:0.0}MB\nFPS:{2:0.0}", totalMemory / 1024.0f / 1024.0f, monoMemory / 1024.0f / 1024.0f, m_fps);
+		}
+	}
+
+	private static Rect m_place = new Rect(0.0f, 0.0f, 300, 100);
+	private static string m_info = string.Empty;
+	public static void OnGUI()
+	{
+		GUI.Label(m_place, m_info);
+	}
+	public static string GetInfo()
+	{
+		return m_info;
+	}
+}
---
+++ ShowProfileMono.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+//用于显示游戏运行帧率和内存占用
+public class ShowProfileMono : MonoBehaviour
+{
+	public float SampleTime = 0.5f;
+	private float m_lastUpdateTime = 0.0f;
+	private float m_fps = 0.0f;
+	private int m_frame = 0;
+	void Update()
+	{
+		++m_frame;
+	}
+	void FixedUpdate()
+	{
+		float currentTime = Time.time;
+		float flameTime = currentTime - m_lastUpdateTime;
+		if (flameTime >= SampleTime)
+		{
+			m_fps = m_frame / flameTime;
+			m_lastUpdateTime = currentTime;
+			m_frame = 0;
+			long monoMemory = System.GC.GetTotalMemory(false);
+			uint totalMemory = Profiler.GetTotalAllocatedMemory();
+			m_info = string.Format("Total:{0:0.0}MB\nMono:{1:0.0}MB\nFPS:{2:0.0}", totalMemory / 1024.0f / 1024.0f, monoMemory / 1024.0f / 1024.0f, m_fps);
+		}
+	}
+
+	private Rect m_place = new Rect(0.0f, 0.0f, 300, 100);
+	private string m_info = string.Empty;
+	void OnGUI()
+	{
+		GUI.Label(m_place, m_info);
+	}
+}
