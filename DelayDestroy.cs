5af04c36d94c9f70ec83e374f284c13985d6db31
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 14:19:04 +0800

    添加延迟销毁工具

---
+++ DelayDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DelayDestroy : MonoBehaviour
+{
+	public float Delay;
+	IEnumerator Start()
+	{
+		Debug.LogFormat("{0}", Time.time);
+		yield return new WaitForSeconds(Delay);
+		Debug.LogFormat("{0}", Time.time);
+		Object.Destroy(gameObject);
+	}
+}
