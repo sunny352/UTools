31642085ecd72806bd6c03cfee298f5d82cc6185
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 13:35:00 +0800

    添加弹性追踪功能

---
+++ Bounce.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Bounce
+{
+	public Transform Target;
+	public float Value;
+	public float MaxSpeed;
+	public float MinSpeed;
+
+	private Transform transform;
+	public void Enable()
+	{
+		m_isEnable = true;
+	}
+	public void Disable()
+	{
+		m_isEnable = false;
+	}
+	private bool m_isEnable = true;
+	public void Init(Transform selfTrans)
+	{
+		transform = selfTrans;
+	}
+	public void LateUpdate()
+	{
+		if (null == Target)
+		{
+			return;
+		}
+		var distance = Target.position - transform.position;
+		float speed = Mathf.Clamp(distance.sqrMagnitude * Value, MinSpeed, MaxSpeed);
+		var transPos = Time.deltaTime * speed * distance.normalized;
+		if (transPos.sqrMagnitude < distance.sqrMagnitude)
+		{
+			transform.Translate(transPos, Space.World);
+		}
+		else
+		{
+			transform.position = Target.position;
+		}
+	}
+}
---
+++ BounceMono.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BounceMono : MonoBehaviour
+{
+	public Transform Target;
+	public float Value;
+	public float MaxSpeed;
+	public float MinSpeed;
+	void LateUpdate()
+	{
+		if (null == Target)
+		{
+			return;
+		}
+		var distance = Target.position - transform.position;
+		float speed = Mathf.Clamp(distance.sqrMagnitude * Value, MinSpeed, MaxSpeed);
+		var transPos = Time.deltaTime * speed * distance.normalized;
+		if (transPos.sqrMagnitude < distance.sqrMagnitude)
+		{
+			transform.Translate(transPos, Space.World);
+		}
+		else
+		{
+			transform.position = Target.position;
+		}
+	}
+}
