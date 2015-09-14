fa6810958fdc2ce8590d0375cbadc85979d9c0d8
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 14:51:22 +0800

    移除log

--- DelayDestroy.cs
+++ DelayDestroy.cs
@@ -6,9 +6,7 @@ public class DelayDestroy : MonoBehaviour
 	public float Delay;
 	IEnumerator Start()
 	{
-		Debug.LogFormat("{0}", Time.time);
 		yield return new WaitForSeconds(Delay);
-		Debug.LogFormat("{0}", Time.time);
 		Object.Destroy(gameObject);
 	}
 }
