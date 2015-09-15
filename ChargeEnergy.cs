12fbb4242cfa716cee587c841b57bf27931f24f6
sunny352@gmail.com <sunny352@gmail.com>
2015-09-15 13:35:29 +0800

    添加蓄力功能

---
+++ ChargeEnergy.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ChargeEnergy
+{
+	public float PreTime;
+	public float Duration;
+	public int MaxLevel;
+	public bool IsFireOnFinished = false;
+	public bool IsFireOnEnd = true;
+
+	public Action OnChargeStart;
+	public Action<int> OnChargeFire;
+	public Action<int> OnChargeLevelUp;
+	public Action OnChargeFinished;
+	public Action OnChargeInterupt;
+	public void OnBegin()
+	{
+		Debug.Log("OnBegin");
+		m_isCharging = true;
+		m_beginTime = Time.time;
+	}
+	public void OnEnd()
+	{
+		Debug.Log("OnEnd");
+		if (m_isCharging)
+		{
+			if (IsFireOnEnd)
+			{
+				OnFire(Level);
+			}
+		}
+		Reset();
+	}
+	public void OnInterupt()
+	{
+		m_isCharging = false;
+		m_beginTime = 0.0f;
+		if (null != OnChargeInterupt)
+		{
+			OnChargeInterupt();
+		}
+		Reset();
+	}
+	public void Reset()
+	{
+		m_isCharging = false;
+		m_beginTime = 0.0f;
+		m_preLevel = 0;
+	}
+	private float m_beginTime;
+	private bool m_isCharging;
+	public int Level
+	{
+		get
+		{
+			float currentTime = Time.time;
+			float chargeDuration = currentTime - m_beginTime - PreTime;
+			if (chargeDuration < 0)
+			{
+				return 0;
+			}
+			int level = (int)(chargeDuration / Duration) + 1;
+			return Mathf.Min(level, MaxLevel);
+		}
+	}
+	public bool IsCharging { get { return m_isCharging; } }
+	private int m_preLevel = 0;
+	public void FixedUpdate()
+	{
+		if (m_isCharging)
+		{
+			int currentLevel = Level;
+			if (currentLevel != m_preLevel)
+			{
+				m_preLevel = currentLevel;
+				if (1 == currentLevel)
+				{
+					OnStart();
+				}
+				OnLevelUp(currentLevel);
+				if (currentLevel == MaxLevel)
+				{
+					OnFinished();
+				}
+			}
+		}
+	}
+	private void OnStart()
+	{
+		if (null != OnChargeStart)
+		{
+			OnChargeStart();
+		}
+	}
+	private void OnFire(int currentLevel)
+	{
+		if (null != OnChargeFire)
+		{
+			OnChargeFire(currentLevel);
+		}
+	}
+	private void OnLevelUp(int currentLevel)
+	{
+		if (null != OnChargeLevelUp)
+		{
+			OnChargeLevelUp(currentLevel);
+		}
+	}
+	private void OnFinished()
+	{
+		if (null != OnChargeFinished)
+		{
+			OnChargeFinished();
+		}
+		if (IsFireOnFinished)
+		{
+			OnFire(Level);
+			Reset();
+		}
+	}
+}
---
+++ ChargeEnergyMono.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeEnergyMono : MonoBehaviour
+{
+	public float PreTime;
+	public float Duration;
+	public int MaxLevel;
+	public GameObject Target;
+	public bool IsFireOnFinished = false;
+	public bool IsFireOnEnd = true;
+	void OnBegin()
+	{
+		Debug.Log("OnBegin");
+		m_isCharging = true;
+		m_beginTime = Time.time;
+	}
+	void OnEnd()
+	{
+		Debug.Log("OnEnd");
+		if (m_isCharging)
+		{
+			if (IsFireOnEnd)
+			{
+				OnFire(Level);
+			}
+		}
+		Reset();
+	}
+	void OnInterupt()
+	{
+		m_isCharging = false;
+		m_beginTime = 0.0f;
+		if (null != Target)
+		{
+			Target.SendMessage("OnChargeInterupt", SendMessageOptions.DontRequireReceiver);
+		}
+		Reset();
+	}
+	public void Reset()
+	{
+		m_isCharging = false;
+		m_beginTime = 0.0f;
+		m_preLevel = 0;
+	}
+	private float m_beginTime;
+	private bool m_isCharging;
+	public int Level
+	{
+		get
+		{
+			float currentTime = Time.time;
+			float chargeDuration = currentTime - m_beginTime - PreTime;
+			if (chargeDuration < 0)
+			{
+				return 0;
+			}
+			int level = (int)(chargeDuration / Duration) + 1;
+			return Mathf.Min(level, MaxLevel);
+		}
+	}
+	public bool IsCharging { get { return m_isCharging; } }
+	private int m_preLevel = 0;
+	void FixedUpdate()
+	{
+		if (m_isCharging)
+		{
+			int currentLevel = Level;
+			if (currentLevel != m_preLevel)
+			{
+				m_preLevel = currentLevel;
+				if (1 == currentLevel)
+				{
+					OnStart();
+				}
+				OnLevelUp(currentLevel);
+				if (currentLevel == MaxLevel)
+				{
+					OnFinished();
+				}
+			}
+		}
+	}
+	private void OnStart()
+	{
+		if (null != Target)
+		{
+			Target.SendMessage("OnChargeStart", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	private void OnFire(int currentLevel)
+	{
+		if (null != Target)
+		{
+			Target.SendMessage("OnChargeFire", currentLevel, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	private void OnLevelUp(int currentLevel)
+	{
+		if (null != Target)
+		{
+			Target.SendMessage("OnChargeLevelUp", currentLevel, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	private void OnFinished()
+	{
+		if (null != Target)
+		{
+			Target.SendMessage("OnChargeFinished", SendMessageOptions.DontRequireReceiver);
+		}
+		if (IsFireOnFinished)
+		{
+			OnFire(Level);
+			Reset();
+		}
+	}
+}
